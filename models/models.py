0a776de78e6a540c95e9dd5aaaad438968a0a989
Serhii <yankovsergiy@gmail.com>
2017-11-05 21:57:42 +0200

    Relational fields

--- models/models.py
+++ models/models.py
@@ -10,6 +10,9 @@ class Course(models.Model):
 
     responsible_id = fields.Many2one('res.users',
         ondelete='set null', string="Responsible", index=True)
+    session_ids = fields.One2many(
+        'openacademy.session', 'course_id', string="Sessions")
+
 
 
 class Session(models.Model):
@@ -23,7 +26,7 @@ class Session(models.Model):
     instructor_id = fields.Many2one('res.partner', string="Instructor")
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
-
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
 
 # class academy_course_module(models.Model):
